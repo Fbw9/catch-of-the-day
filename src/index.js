ffc8245b3c1a5ccb04fa36a0fb4e8d3f3ed00dc0
Ryan Kretch <rkretch91@gmail.com>
2018-10-09 11:09:09 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import "./css/style.css";
-import React from "react";
-import { render } from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import { render } from 'react-dom';
+import './css/style.css';
+import App from './components/App';
 
-render(< App />, document.querySelector("#main")); 
+import StorePicker from './components/StorePicker';
+
+render(<App/>, document.querySelector('#main'));
