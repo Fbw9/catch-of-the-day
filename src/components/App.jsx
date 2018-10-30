e762f38d3cab191ec621108d625ed700aa53e58b
Ryan Kretch <rkretch91@gmail.com>
2018-10-30 13:45:24 +0100

    state now displays

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish'
 import sampleFishes from '../sample-fishes'
 
 
@@ -14,24 +15,28 @@ class App extends React.Component {
     }
   }
   addFish = fish => {
-    let fishes = {...this.state.fishes};
+    let fishes = { ...this.state.fishes };
     const time = Date.now();
     fishes[`fish-${time}`] = fish
-    this.setState({fishes})
+    this.setState({ fishes })
   }
 
   loadSamples = () => {
-    this.setState({fishes: sampleFishes})
+    this.setState({ fishes: sampleFishes })
   }
 
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header/>
+          <Header />
+          <ul className="list-of-fishes">
+            {Object.keys(this.state.fishes)
+            .map(id => <Fish key={id} item={this.state.fishes[id]} />)}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
